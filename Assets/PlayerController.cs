89d3389191ce6e841f33458703151ab1f12fe6fa
Sebastian Holmgren <seho3066@gmail.com>
2018-11-01 18:35:11 +0100

    Testing git for unity

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private float jumpTimer;
+    float newPosX;
+    float newPosY;
+    public Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector3.up * 50 * Time.deltaTime, Space.World);
+
+
+
+        }
+
+        newPosX = transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.position = new Vector3(newPosX, transform.position.y);
+	}
+}
