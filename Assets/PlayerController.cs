c49945e8276c14dc52ae434002d3eb059621c01a
Sebastian Holmgren <seho3066@gmail.com>
2018-11-02 23:02:17 +0100

    Added rough core mechanics

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,28 +4,70 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public LayerMask jumpingMask;
+    public LayerMask waterMask;
+    public float thrust;
     public float speed;
-    private float jumpTimer;
-    float newPosX;
-    float newPosY;
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Vector3 spawnPosition;
+    public float status = 0;
+
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
+        spawnPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown("space"))
-        {
-            transform.Translate(Vector3.up * 50 * Time.deltaTime, Space.World);
+        float current_speed = speed;
+        float avaible_thrust = thrust;
 
-
-
+        if (InWater()){
+            current_speed *= .5f;
+            avaible_thrust *= .2f;
+            rb.gravityScale = .5f;
+            
+            status += Time.deltaTime;
+        } else {
+            rb.gravityScale = 2f;
+            status -= Time.deltaTime;
         }
 
-        newPosX = transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (Input.GetKeyDown("space"))
+        {
+            if (CanJump() || InWater()) {
+                rb.AddForce(transform.up * avaible_thrust);
+            }
+        }
+
+        float newPosX = transform.position.x + Input.GetAxis("Horizontal") * current_speed * Time.deltaTime;
         transform.position = new Vector3(newPosX, transform.position.y);
 	}
+
+    bool CanJump() {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 1f, jumpingMask);
+ 
+        if (hit.collider != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool InWater(){
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .5f, waterMask);
+
+        if (hit.collider != null) {
+            Debug.Log("In water");
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Respawn() {
+        transform.position = spawnPosition;
+    }
 }
