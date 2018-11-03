8d20c1a666189c44e6a5349cd0bd92a1765881fd
Jacob Johansson <jikan_j@hotmail.com>
2018-11-03 16:51:18 +0100

    Refcator

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Vector3 spawnPosition;
+    CharacterController character;
+
+    private void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    void Update () {
+
+        character.Move(Input.GetAxis("Horizontal"));
+
+        if (Input.GetKeyDown("space"))
+        {
+            character.Jump();
+        }
+    }
+
+    public void Respawn() {
+        transform.position = spawnPosition;
+    }
+}
---
+++ Assets/TriggerVolume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerVolume : MonoBehaviour {
+
+	public TriggerType VolumeType;
+
+	public GameManager gameManager;
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        gameManager.RegisterTrigger(VolumeType, col.gameObject);
+    }
+}
+
+public enum TriggerType {
+	Win,
+	Lose
+}
