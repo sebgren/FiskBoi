4bc2471067698216703f442399674ec89983cd31
Joakim <joakim.bostrom43@gmail.com>
2018-11-04 05:08:50 +0100

    Added player and npc stats classes, added death and damgage events

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	public void RegisterTrigger(TriggerType e_type, GameObject player) {
+    [SerializeField]
+    private GameObject _player;
+
+    private PlayerStats _PlayerStats;
+
+    public void Start()
+    {
+        if (_player == null)
+        {
+            throw new MissingComponentException("Player is not set!");
+        }
+
+        _PlayerStats = _player.GetComponent<PlayerStats>();
+
+        // TODO Add death reason
+        _PlayerStats.deathEvent += () =>
+        {
+            gameOver();
+        };
+
+    }
+
+
+    // TODO Move this trigger event to the player controller
+    public void RegisterTrigger(TriggerType e_type, GameObject player) {
 		if (e_type == TriggerType.Lose) { 
-			Debug.Log("You lost!"); 
-			player.GetComponent<PlayerController>().Respawn();
+			player.GetComponent<PlayerStats>().die();
 		}
 		if (e_type == TriggerType.Win) { Debug.Log("You win!"); }
 	}
+
+    private void gameOver()
+    {
+        Debug.Log("You Died!");
+        _player.GetComponent<PlayerController>().Respawn();
+    }
+
 }
