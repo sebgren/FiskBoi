1f188e97a72a2b0d23c1189b0344f6ed1118cd2c
Joakim <joakim.bostrom43@gmail.com>
2018-11-06 00:03:57 +0100

    added funk

---
+++ Assets/Scripts/Character/CharacterSounds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSounds : MonoBehaviour {
+
+    [SerializeField]
+    AudioSource _characterSoundSource;
+
+    [SerializeField]
+    AudioClip _jump;
+
+	// Use this for initialization
+	void Start () {
+		_characterSoundSource = GetComponent<AudioSource>();
+
+        if (_characterSoundSource == null)
+        {
+            throw new MissingComponentException("Missing AudioSource");
+        }
+
+        if (_jump == null)
+        {
+            Debug.LogWarning("No Jump Sound found. No sounds will play when jumping");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void jump()
+    {
+        _characterSoundSource.PlayOneShot(_jump);
+    }
+}
