d90cae6def71f512560cbe32cdf24f7294c0ff5d
Joakim <joakim.bostrom43@gmail.com>
2018-11-10 03:14:17 +0100

    added death menu

---
+++ Assets/Scripts/UI/RespawnOnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOnClick : MonoBehaviour
+{
+    GameManager _gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject manager = GameObject.Find("GameManager");
+
+        if (!manager)
+        {
+            throw new MissingReferenceException("There is no GameManager in the scene?!?!");
+        }
+        _gameManager = manager.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickRespawn()
+    {
+        _gameManager.respawn();
+    }
+}
