e2385397b97943628529cd80aef6947b89d766d2
Joakim <joakim.bostrom43@gmail.com>
2018-12-01 14:00:55 +0100

    final commit

---
+++ Assets/Scripts/UI/TheBar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheBar : MonoBehaviour
+
+{
+    [SerializeField]
+    PlayerStats stats;
+
+    [SerializeField]
+    GameManager manager;
+    [SerializeField]
+    Text timer;
+
+    [SerializeField]
+    Image left;
+    [SerializeField]
+    Image right;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        left = GameObject.Find("LeftFill").GetComponent<Image>();
+        right = GameObject.Find("RightFill").GetComponent<Image>();
+        stats.breathEvent += (target, breath) =>
+        {
+            var adjustedBreath = breath + 10;
+            adjustedBreath = adjustedBreath / 20;
+            left.fillAmount = 1 - adjustedBreath;
+            right.fillAmount = adjustedBreath;
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.text = Math.Round(manager._timer, 2).ToString();
+    }
+}
---
+++ Assets/Scripts/UI/VictorySCreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class VictorySCreen : MonoBehaviour
+{
+
+    GameManager manager;
+
+    [SerializeField]
+    Text hs;
+    [SerializeField]
+    Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        manager.onGameStateChange += (GameManager.GameState newState, GameManager.GameState oldState) =>
+        {
+            hs.text = "HighScore " + Math.Round(PlayerPrefs.GetFloat("HS" + SceneManager.GetActiveScene().buildIndex), 2).ToString();
+        };
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Score " + Math.Round(manager._timer, 2).ToString();
+    }
+}
